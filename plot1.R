54e2d7a28af0a6a0ee59edbae9b86a66d89a57b1
SAKTHI SIVRAM <psakthisivram@gmail.com>
2016-06-17 23:57:26 -0400

    plot 1 and 2

--- plot1.R
+++ plot1.R
@@ -15,9 +15,17 @@ if (!file.exists("household_power_consumption.txt")) {
   unzip(filename) 
 }
 
+# extract header names
 header <- strsplit(readLines("./household_power_consumption.txt", n = 1), ";")
+
+# filter feb 1st and 2nd data
 febData <- read.table("./household_power_consumption.txt",skip=66636, nrows = 2880, header = TRUE, sep = ";", na.strings = c("?"), col.names = c(unlist(header)))
 
+# open device
 png(file="plot1.png",width=480,height=480)
+
+# create histogram for global active power
 hist(febData$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+#turnoff device
 dev.off()
--- plot2.R
+++ plot2.R
@@ -14,9 +14,18 @@ if (!file.exists("household_power_consumption.txt")) {
   unzip(filename) 
 }
 
+# extract header names
 header <- strsplit(readLines("./household_power_consumption.txt", n = 1), ";")
+
+# filter feb 1st and 2nd data
 febData <- read.table("./household_power_consumption.txt",skip=66636, nrows = 2880, header = TRUE, sep = ";", na.strings = c("?"), col.names = c(unlist(header)))
+
+# concat data and time
 febData$datetime <- strptime(paste(febData$Date, febData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+# open device
 png(file="plot2.png",width=480,height=480)
+
+# create plot between datetime and global active power
 plot(febData$datetime, as.numeric(febData$Global_active_power), type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 dev.off()
